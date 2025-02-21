e2de94ec4f985b5737fc4b1a840e145649ab6dea
Kiri-06 <powersun08@gmail.com>
2025-02-21 14:36:12 +0900

    Update Program.cs

--- Project/ConsoleApp1/Program.cs
+++ Project/ConsoleApp1/Program.cs
@@ -30,33 +30,78 @@ namespace Program
             // Console.WriteLine("Pi : " + Pi);
             // // !error to "Pi = 3.14;"
 
-            //기본 특성
-            int Attack = 16755;
-            int MaxHealth = 78103;
+            // //기본 특성
+            // int Attack = 16755;
+            // int MaxHealth = 78103;
 
-            //전투 특성
-            int trait_Fatal = 36; //치명
-            int trait_Special = 1017; //특화
-            int trait_Subdue = 41; //제압
-            int trait_Swift = 611; //신속
-            int trait_Persev = 22; //인내
-            int trait_Train = 39; //숙련
+            // //전투 특성
+            // int trait_Fatal = 36; //치명
+            // int trait_Special = 1017; //특화
+            // int trait_Subdue = 41; //제압
+            // int trait_Swift = 611; //신속
+            // int trait_Persev = 22; //인내
+            // int trait_Train = 39; //숙련
 
-            Console.WriteLine("----------기본 특성----------");
-            Console.WriteLine("공격력 : " + Attack);
-            Console.WriteLine("최대 생명력 : " + MaxHealth);
+            // Console.WriteLine("----------기본 특성----------");
+            // Console.WriteLine("공격력 : " + Attack);
+            // Console.WriteLine("최대 생명력 : " + MaxHealth);
 
-            Console.WriteLine("\r\n----------전투 특성----------");
-            Console.WriteLine("치명 : " + trait_Fatal);
-            Console.WriteLine("특화 : " + trait_Special);
-            Console.WriteLine("제압 : " + trait_Subdue);
-            Console.WriteLine("신속 : " + trait_Swift);
-            Console.WriteLine("인내 : " + trait_Persev);
-            Console.WriteLine("숙련 : " + trait_Train);
+            // Console.WriteLine("\r\n----------전투 특성----------");
+            // Console.WriteLine("치명 : " + trait_Fatal);
+            // Console.WriteLine("특화 : " + trait_Special);
+            // Console.WriteLine("제압 : " + trait_Subdue);
+            // Console.WriteLine("신속 : " + trait_Swift);
+            // Console.WriteLine("인내 : " + trait_Persev);
+            // Console.WriteLine("숙련 : " + trait_Train);
 
+            // int intValue = 100; long longValue = 1234567890L; float flValue = 0.4f; double dblValue = 3.1415926535;
+            // Console.WriteLine("intValue : " + intValue);
+            // Console.WriteLine("longValue : " + longValue);
+            // Console.WriteLine("flValue : " + flValue);
+            // Console.WriteLine("dblValue : " + dblValue);
 
+            // byte byteValue = 1; //byte는 1~ sbyte가 -n~
+            // ushort ushortValue = 2;
+            // uint uintValue = 9999;
+            // sbyte sbyteValue = -128;
+            // Console.WriteLine("byteValue : " + byteValue);
+            // Console.WriteLine("ushortValue : " + ushortValue);
+            // Console.WriteLine("uintValue : " + uintValue);
+            // Console.WriteLine("sbyteValue : " + sbyteValue);
+
+            // float singlePre = 3.14f;
+            // double doublePre = 3.14;
+            // decimal decimalPre = 3.14m;
+            // Console.WriteLine("singlePre : " + singlePre);
+            // Console.WriteLine("doublePre : " + doublePre);
+            // Console.WriteLine("decimalPre : " + decimalPre);
+
+            // string greeting = "Hello World";
+            // string name = "Alice";
+            // Console.WriteLine(greeting + " " + name);
+
+            // //Console.Write => \n미적용 Console.WriteLine => \n적용
+            // Console.Write("1"); Console.WriteLine("2"); Console.WriteLine("3");
+
+            // bool isGo = false;
+            // Console.WriteLine("isGo : " + isGo);
             
-            
+            Int32 number = 123;
+            String text = "Hello";
+            Boolean flag = true;
+            Console.WriteLine("number : " + number);
+            Console.WriteLine("text : " + text);
+            Console.WriteLine("flag : " + flag);
+
+            int intostr = 123;
+            string intostring = intostr.ToString();
+            Console.WriteLine("intostring : " + intostring);
+            bool flag = true;
+            string flagAsString = flag.ToString();
+            Console.WriteLine("flagAsString : " + flagAsString);
+
+
+
 
         }
     }
