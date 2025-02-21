4fb73c03b878399b1d421c135ec12945f872b024
Kiri-06 <powersun08@gmail.com>
2025-02-21 10:55:47 +0900

    Update Program.cs

--- Project/ConsoleApp1/Program.cs
+++ Project/ConsoleApp1/Program.cs
@@ -6,7 +6,20 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("출력합니다");
+            //캐릭터
+            //hp
+            //att
+            //캐릭터이름
+            //등급
+            double cht_hp = 100;
+            double cht_att = 56.7;
+            string cht_name = "Tomas";
+            char cht_rank = 'S';
+            Console.WriteLine("Character");
+            Console.WriteLine("Hp : " + cht_hp);
+            Console.WriteLine("Att : " + cht_att);
+            Console.WriteLine("Name : " + cht_name);
+            Console.WriteLine("Rank : " + cht_rank);
         }
     }
 }
